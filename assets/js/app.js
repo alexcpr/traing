a271989efac14c6e1bdd5f4f2f393521e775b997
Alexandre CIPOR <contact@alexandrecipor.com>
2023-11-22 11:41:56 +0100

    improvement: ignore hyphens when searching station

--- assets/js/app.js
+++ assets/js/app.js
@@ -1313,7 +1313,10 @@ function renderStationButton(stationName, isFavorite) {
 }
 
 function removeAccents(str) {
-  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  return str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/-/g, "");
 }
 
 function renderNonFavoriteStationButtons() {
